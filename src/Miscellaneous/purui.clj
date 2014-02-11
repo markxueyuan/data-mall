08e11572fa0eb22700079abd6c90f6e522755033
markxueyuan <ziyaajiang@gmail.com>
2014-02-11 18:47:01 +0800

    finally solve the db lazy io problem

--- src/Miscellaneous/purui.clj
+++ src/Miscellaneous/purui.clj
@@ -2,14 +2,12 @@
   (:require [data-mall.lazyProcess :as lz]
             [incanter.core :as incanter]
             [clojure.java.jdbc :as jdbc]
-            [data-mall.toCSVorJSON :as toCSV]))
+            [data-mall.toCSVorJSON :as toCSV]
+            [clojure.java.io :as javaio]
+            [clojure.string :as string]
+            ))
 
-(def db-spec
-  {:classname "com.mysql.jdbc.Driver"
-   :subprotocol "mysql"
-   :subname "//localhost:3306/doubancomment"
-   :user "root"
-   :password "othniel"})
+;;;;;;;;;;;db connection;;;;;;;;
 
 (def db-spec2
   {:classname "com.mysql.jdbc.Driver"
@@ -18,29 +16,96 @@
    :user "root"
    :password "othniel"})
 
-(jdbc/query db-spec2 ["select * from previews"]
+;;;;;;;;;;;functions;;;;;;;;
+
+(defn ->csv
+  [file resultset]
+  (toCSV/toCSV (incanter/to-dataset resultset) file))
+
+(defn cut-date-string
+  [date-string]
+  (first (string/split date-string #"\s")))
+
+(defn extract-date
+  [identifier entry]
+  (let [tkey (keyword identifier)]
+    (assoc entry tkey (cut-date-string (tkey entry)))))
+
+;;;;;;;;csv file address ;;;;;;;
+
+(def address-1 "E:/数据/朴睿/analysis.csv")
+
+;;;;;;zone of sqls;;;;;;;
+
+(def query-1 "select topic, count(*) as vol from purui0208_2013_spam_dupliremov group by topic;")
+(def query-2 "select kw, topic, count(*) as vol from purui0208_2013_spam_dupliremov group by kw order by topic, kw;")
+(def query-3 "select 'cartier', pubtime FROM purui0208_2013_spam_dupliremov")
+
+
+;;;;;;;;;working area;;;;;;;
+
+
+;group by topic
+
+(->> (jdbc/query db-spec2 [query-1])
+     (->csv address-1)
+     )
+
+;group by keyword
+
+(->> (jdbc/query db-spec2 [query-2])
+     (->csv address-1)
+     )
+
+;cartier's distribution by time
+
+
+
+(->> (jdbc/query db-spec2 [query-3])
+     (map #(extract-date "pubtime" %))
+     frequencies
+     (map #(assoc (first %) :counts (second %)));mimic a standard jdbc output
+     (sort #(compare (:pubtime %1) (:pubtime %2)))
+     (->csv address-1)
+     )
+
+
+
+(jdbc/query db-spec2 [query-3])
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;tips;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;do calculation lazily
+
+#_(jdbc/query db-spec2 ["select * from previews"]
             :row-fn :similar_count
             :result-set-fn frequencies)
 
-(toCSV)
+
+;return vector
+
+#_(def a (jdbc/query db-spec2 ["SELECT origin, count(*) from previews group by origin order by count(*) desc;"]
+                     :as-arrays? true))
 
 
+;insert rows
+#_(jdbc/insert! db-spec2 :previews {:origin "大纪元" :preview "她是个好同志"})
+
+;update rows
+#_(jdbc/update! db-spec2 :previews {:preview "她是个大坏蛋" :title "鬼吹灯系列"} ["origin = ?" "大纪元"])
+
+;delete rows
+
+#_(jdbc/delete! db-spec2 :previews ["origin = ?" "大纪元"])
+
+;rename col name
+#_(jdbc/query db-spec2 ["select * from previews limit 20;"]
+            :identifiers #(.replace % \_ \-)
+            )
 
 
-
-(reduce  + (Integer. "3") [1 2 3])
-
-
-
-;(map :pubtime (lz/lazy-read-db db-spec2 'previews))
-
-
-
-;(frequencies (map :text (lz-db/lazy-read-db db-spec 'previews)))
-
-
-;(lz/lazy-read-csv "D:/data/previews.csv")
-;(def data (drop 1 (lz/lazy-read-csv "D:/data/previews.csv")))
-
-;(count (map #(zipmap [:origin :preview :pubtime :similar_count :title :url] %) data))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;not that smart;;;;;;;;;;;;;;;;;;;;;;;;
+(string/split "1012-3-4 12:22" #"\s")
