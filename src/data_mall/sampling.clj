8bf2fe87b0724ece44496e303ed31770b40df3e6
Yuan Xue <ziyaajiang@gmail.com>
2014-02-17 04:57:40 +0800

    suddenly wake up at mid-night

--- src/data_mall/sampling.clj
+++ src/data_mall/sampling.clj
@@ -1,6 +1,31 @@
-(ns data-mall.sampling)
+(ns data-mall.sampling
+  (:require [clojure.java.jdbc :as jdbc]))
+
+;;;;;;;;;;;;;;db-connection;;;;;;;;;;;;;;;;;
+
+(def db-spec1
+  {:classname "com.mysql.jdbc.Driver"
+   :subprotocol "mysql"
+   :subname "//localhost:3306/doubancomment"
+   :user "root"
+   :password "othniel"})
+
+
+;;;;;;;;;;;;;;fns;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn sample-percent
   [percent coll]
   (filter (fn [_] (<= (rand) percent)) coll))
+
+
+;;;;;;;;;;;;;;sqls;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def query-1 "SELECT * FROM dfl;")
+
+;;;;;;;;;;;;;;;working area;;;;;;;;;;;;;;;;;;
+
+#_(->> (jdbc/query db-spec1 [query-1])
+     (sample-percent 0.1)
+     count
+     )
 
