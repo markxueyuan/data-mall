feafb64a023cdf62f2ad9118fa03cd6708058186
Yuan Xue <ziyaajiang@gmail.com>
2014-02-20 00:26:09 +0800

    spelling error checking code is understood. I'd like to lick the shoes of this code's author.

--- src/data_mall/spell_error.clj
+++ src/data_mall/spell_error.clj
@@ -35,16 +35,51 @@
   (let [w2-0 (.substring w2 1)]
     (map #(str w1 % w2-0) alphabet)))
 
+(defn insert-split
+  [[w1 w2]]
+  (map #(str w1 % w2) alphabet))
+
 (defn edits-1
   [word]
   (let [splits (map (partial split-word word) (range (inc (count word))))
         long-splits (filter #(> (count (second %)) 1) splits)
         deletes (map delete-char long-splits)
         transposes (map transpose-split long-splits)
-        ]
-    )
-  )
+        replaces (mapcat replace-split long-splits)
+        inserts (remove nil? (mapcat insert-split splits))] ;don't know why need a remove here
+    (set (concat deletes transposes replaces inserts))))
 
-;(words "Mama takes good care of me.")
+(defn known-edits-2
+  [word]
+  (set (filter (partial contains? n-words)
+               (apply union (map #(edits-1 %) (edits-1 word))))))
+
+(defn known
+  [words]
+  (set (filter (partial contains? n-words) words)))
+
+(defn correct
+  [word]
+  (let [candidate-thunks [#(known (list word))
+                          #(known (edits-1 word))
+                          #(known-edits-2 word)
+                          #(list word)]]
+    (->> candidate-thunks
+         (map (fn [f] (f)))
+         (filter #(> (count %) 0))
+         first
+         (map (fn [w] [(get n-words w 1) w]))
+         (reduce (partial max-key first))
+         second)))
+
+
+
+
+
+
+
+
+
+
 
 
