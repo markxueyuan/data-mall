990c41f4c1037f5280b4d5fab3b9b82ce48b8ff7
markxueyuan <ziyaajiang@gmail.com>
2014-02-24 18:06:13 +0800

    rutine

--- src/data_mall/untitled.clj
+++ src/data_mall/untitled.clj
@@ -3,63 +3,18 @@
          '[incanter.io :as io]
          '[incanter.core :as incanter])
 
-(def data-file "D:/data/all_160_in_51.P35.csv")
+(future (apply + (range 1e8)))
 
-(-> data-file
-    (io/read-dataset :header true)
-    (incanter/view))
+(def long-cal (future (apply + (range 1e8))))
 
-(def total-hu (ref 0))
+@long-cal
+@(future (Thread/sleep 5000) :done!)
+@long-cal
 
-(def total-fams (ref 0))
-
-(defn with-header [coll]
-  (let [headers (map keyword (first coll))]
-    (map (partial zipmap headers) (next coll))))
-
-(defn ->int ([i] (Integer. i)))
-
-(defn sum-item
-  ([fields] (partial sum-item fields))
-  ([fields accum item]
-   (mapv + accum (map ->int (map item fields)))))
-
-(defn sum-items
-  [accum fields coll]
-  (reduce (sum-item fields) accum coll))
-
-(defn update-totals [fields items]
-  (let [mzero (map (constantly 0) fields)
-        [sum-hu sum-fams] (sum-items mzero fields items)]
-    (dosync (alter total-hu #(+ sum-hu %))
-            (alter total-fams #(+ sum-fams %)))))
-
-(defn thunk-update-totals-for
-  [fields data-chunk]
-  (fn [] (update-totals fields data-chunk)))
-
-(defn main
-  ([data-file] (main data-file [:HU100 :P035001] 5))
-  ([data-file fields chunk-count]
-   (doall)))
+(apply + (range 1e8))
 
 
 
 
 
 
-;;;;;;;;;;;;;;;;;tips;;;;;;;;;;;;;;;
-
-;turn string to integer
-
-(Integer. "8")
-
-;how to make a constant function?
-
-(constantly 0)
-
-
-
-
-
-
