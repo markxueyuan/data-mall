72fa123cc2cafda539625c55d485a702c2fc2118
markxueyuan <ziyaajiang@gmail.com>
2014-02-27 18:13:02 +0800

    added some codes for transfering youtube sublines to srt form and reviewd the time package for clojure

--- src/Miscellaneous/make_src.clj
+++ src/Miscellaneous/make_src.clj
@@ -1,1 +1,57 @@
-(ns Miscellaneous.make-src)
+(ns Miscellaneous.make-src
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(defn to-srt
+  "This turns youtube subtitles form into srt form"
+  [file]
+  (let [tuples (->> file
+                    slurp
+                    ((fn [s] (string/split s #"\r\n")))
+                    (drop 1)
+                    (partition-all 2))
+        end-time (->> tuples
+                      (drop 1)
+                      (map first))
+        start-time (->> tuples
+                        butlast
+                        (map first))
+        subtitles (->> tuples
+                      butlast
+                      (map second))
+        timelines (map #(str "00:" %1 ",200" " --> " "00:" %2 ",000") start-time end-time)
+        ids (map inc (range (count timelines)))
+        func (fn [id timeline subtitle] (str id "\r\n" timeline "\r\n" subtitle "\r\n\r\n"))
+        seqs (map func ids timelines subtitles)]
+    (apply str seqs)))
+
+
+(defn print-it
+  [writer]
+  #(binding [*out* writer]
+     (print %)))
+
+(defn file-to
+  [file]
+  #(with-open [f (io/writer file :append false)]
+     ((print-it f) %)))
+
+
+(defn ->file [address-from address-to]
+  ((file-to address-to) (to-src address-from)))
+
+(defn ->file2 [address-from]
+  (->file address-from (str address-from ".srt")))
+
+;;;;;;;;;;;;;;;;;;working-area;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+#_(->file2 "E:/mongoDB_courses/welcome")
+
+
+
+
+
+
---
+++ src/language/time.clj
@@ -0,0 +1,39 @@
+(ns language.time
+  (:require [clj-time.core :as t]
+            [clj-time.format :as f]))
+
+(t/date-time 1986 4 2 13 22 4 789)
+
+(t/date-time 1986)
+
+(t/date-time 1986 04 2)
+
+(t/now)
+
+(t/epoch)
+
+(t/hour (t/date-time 1987 9 8 2 2 2))
+
+(t/from-time-zone (t/date-time 1983 5 18) (t/time-zone-for-offset -8))
+
+(t/to-time-zone (t/date-time 1983 5 18) (t/time-zone-for-offset -8))
+
+;看看此时此刻系统记录的北京时间是多少
+(t/to-time-zone (t/now) (t/time-zone-for-offset +8))
+
+(t/local-date 2013 3 20)
+
+(t/after? (t/date-time 1986 10) (t/date-time 1986 9))
+
+(t/plus (t/date-time 1986 10 14) (t/months 1) (t/weeks 3))
+
+(t/in-minutes (t/interval (t/date-time 1986 10 2) (t/date-time 1986 10 14)))
+
+(t/in-seconds (t/interval (t/date-time 1986 10 2) (t/date-time 1986 10 14)))
+
+(f/show-formatters)
+
+
+
+
+
