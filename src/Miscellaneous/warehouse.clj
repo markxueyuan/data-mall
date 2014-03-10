117999b624ef0c0e50a791b419dd587846e6a7ad
markxueyuan <ziyaajiang@gmail.com>
2014-03-10 17:59:18 +0800

    start the warehouse job

--- src/Miscellaneous/warehouse.clj
+++ src/Miscellaneous/warehouse.clj
@@ -1,4 +1,4 @@
-(ns Miscellaneous.shzh
+(ns Miscellaneous.warehouse
   (:refer-clojure :exclude [sort find])
   (:require [clojure.string :as string]
             [incanter.core :as incanter]
@@ -12,15 +12,46 @@
   (:import [com.mongodb MongoOptions ServerAddress WriteConcern];the following two is for mongo use
            org.bson.types.ObjectId))
 
-;(mg/connect! {:host "192.168.3.53" :port 7017})
+(mg/connect! {:host "192.168.3.53" :port 7017})
 
-;(mg/set-db! (mg/get-db "test"))
+(mg/set-db! (mg/get-db "test"))
 
-;(mc/find-maps "star_baidunews_history")
+(declare extract-text)
+
+(defn integrate-text
+  [& {:as source}]
+  (let [m [:tianya :tieba :weibo :douban :youku]
+        s (set (keys source))
+        job (filter s m)]
+    (mapcat #(extract-text % (get source %)) job)))
+
+(integrate-text :tianya 3 :douban 2 :tieba "star_baidutieba_contents")
+
+(defn extract-text
+  [source-key source-address]
+  (cond (= source-key :tianya) (println "This is tianya")
+        (= source-key :tieba) (mc/find-maps source-address)
+        (= source-key :weibo) (println "This is weibo")
+        (= source-key :douban) (println "This is douban")
+        (= source-key :youku) (println "This is youku")))
+
+(defn extract-tianya
+  [source-address]
+  (let [m (mc/find-maps source-address)])
+  (select-keys)
+  )
+
+
+
+
+
+(mc/find-maps "star_baidutieba_contents")
 
 #_(with-collection "star_baidunews_history"
   find{})
-(mg/connect!)
-(mg/set-db! (mg/get-db "purui"))
-(pmap #(mc/insert-batch "fulltime" %) (partition-all 50 purui/set6))
+
+
+
+
+
 
