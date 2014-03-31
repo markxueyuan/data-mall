e6bc0b48d1683950ab14967b7d9bbdd169ba3dde
Yuan Xue <ziyaajiang@gmail.com>
2014-04-01 00:58:55 +0800

    more on collection abstraction

--- src/language/colAbstr.clj
+++ src/language/colAbstr.clj
@@ -1,7 +1,5 @@
 (ns language.colAbstr)
 
-(ns language.hello)
-
 (defn scaffold
   [interface]
   (doseq [[iface methods] (->> interface
@@ -12,12 +10,53 @@
                                (group-by first))]
     (println (str " " iface))
     (doseq [[_ name argcount] methods]
-      (println (str " "
+      (println (str "     "
                     (list name (into '[this] (take argcount (repeatedly gensym)))))))))
 
-(map #(.getName %) (.getMethods clojure.lang.IPersistentSet))
 
-(map #(.getDeclaringClass %) (.getMethods clojure.lang.IPersistentSet))
-(map #(.getName (.getDeclaringClass %)) (.getMethods clojure.lang.IPersistentSet))
+(scaffold clojure.lang.IPersistentSet)
+;An array backed set implementation using deftype
 
-(map #(.getParameterTypes %) (.getMethods clojure.lang.IPersistentSet))
+(declare empty-erray-set)
+
+(def ^:private ^:constant max-size 4)
+
+(deftype ArraySet
+  [^objects items ^int size ^unsynchronized-mutable ^int hashcode]
+  clojure.lang.IPersistentSet
+  (get
+   [this x]
+   (loop [i 0]
+     (when (< i size)
+       (if (= x (aget items i))
+         (aget items i)
+         (recur (inc i))))))
+  (contains
+   [this x]
+   (boolean (loop [i 0]
+              (when (< i size)
+                (or (= x (aget items i)) (recur (inc i)))))))
+  (disjoin
+   [this x]
+   (loop [i 0]
+     (if (== i size)
+       this
+       (if (not= x (aget items i))
+         (recur (inc i))
+         (ArraySet. (doto (aclone items)
+                      (aset i (aget items (dec size)))
+                      (aset (dec size) nil))
+                    (dec size)
+                    -1)))))
+  )
+
+
+;;;;;;;;;;;;;;;;;;tips;;;;;;;;;;;;;;;;;;;;;;
+
+;fn boolean coerce to boolean
+
+(boolean "")
+(boolean nil)
+(boolean false)
+(boolean true)
+(boolean {})
