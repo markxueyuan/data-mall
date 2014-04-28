b6f8950f86b5b2ac4de3e39de374b94f84efdb9c
Yuan Xue <ziyaajiang@gmail.com>
2014-04-28 22:33:03 +0800

    hierarchical cluster done

--- src/language/type_hints.clj
+++
@@ -1,9 +0,0 @@
-(ns language.type-hints
-  (:require [criterium.core :as c]))
-
-(defn rand-point [] [(rand) (rand)])
-
-
-(defn center-distance
-  [[x y]]
-  (Math/sqrt (+ (* x x) (* y y))))
--- src/weka/hierarchical.clj
+++ src/weka/hierarchical.clj
@@ -1,11 +1,25 @@
 (ns weka.hierarchical
   (:require [weka.k-means :as macro]
             [weka.readCSV :as r]
+            [weka.column :as c]
             [clojure.string :as s])
   (:import [weka.core EuclideanDistance]
            [weka.clusterers HierarchicalClusterer]))
 
 (def iris (r/load-arff "D:/data/iris.arff"))
 
+
+
 (macro/defanalysis hierarchical HierarchicalClusterer buildClusterer
-                   [["-A" distance EuclideanDistance .getName]])
+                   [["-A" distance EuclideanDistance .getName]
+                    ["-L" link-type :centroid #(s/upper-case (s/replace (name %) \- \_))]
+                    ["-N" k nil :not-nil]
+                    ["-D" verbose false :flag-true]
+                    ["-B" distance-of :node-length :flag-equal :branch-length]
+                    ["-P" print-newick false :flag-true]])
+
+(def iris-petal (c/filter-attributes iris [:sepallength :sepalwidth :class]))
+
+(def hc (hierarchical iris-petal :k 3 :print-newick true))
+
+(.clusterInstance hc (.get iris-petal 149))
